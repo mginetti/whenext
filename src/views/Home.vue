4c9f16e12ec864593c8ccd268fecf45076a5f863
mginetti <matteo.ginetti99@gmail.com>
2021-03-14 21:49:32 +0100

    Change date

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -72,7 +72,7 @@ export default {
   name: "Home",
   data() {
     return {
-      dateArrive: new Date(2021, 3, 10, 11, 30),
+      dateArrive: new Date(2021, 2, 19, 20, 55),
       turn: false,
     };
   },
