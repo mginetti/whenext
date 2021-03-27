93481ef8803be527346321f9d39842f9ee0c8f1a
mginetti <matteo.ginetti99@gmail.com>
2021-03-27 15:43:05 +0100

    Update sw

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import VueCountdownTimer from "vuejs-countdown-timer/";
 Vue.use(VueCountdownTimer);
 Vue.config.productionTip = false;
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      registration.update()
+    }
+  })
+}
+
 new Vue({
   router,
   render: h => h(App)
